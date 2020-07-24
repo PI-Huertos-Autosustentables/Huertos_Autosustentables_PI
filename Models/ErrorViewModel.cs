4f0c5201c0304f39bbbd8505165b50fc121b9844
jhonacosta99 <jhonacosta99@outlook.es>
2020-07-24 00:55:30 -0500

    Primer Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Huertos_Autosustentables_PI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
